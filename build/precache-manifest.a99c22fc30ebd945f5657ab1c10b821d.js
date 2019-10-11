cf286c9980c6acb07a4a5ba2f876109e9ebf6424
SAIAnhNguyen <anh.nguyen@sai-digital.com>
2019-10-11 17:25:27 +0700

    deploy server

---
+++ build/precache-manifest.a99c22fc30ebd945f5657ab1c10b821d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd089659bcb032a63664a18f8b42b3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7029aa718d15c648fbb8",
+    "url": "/static/css/2.2e053283.chunk.css"
+  },
+  {
+    "revision": "fbc69e893998cee8cc8e",
+    "url": "/static/css/main.12f92b1b.chunk.css"
+  },
+  {
+    "revision": "7029aa718d15c648fbb8",
+    "url": "/static/js/2.59a40ade.chunk.js"
+  },
+  {
+    "revision": "fbc69e893998cee8cc8e",
+    "url": "/static/js/main.c7372143.chunk.js"
+  },
+  {
+    "revision": "c1d30bee6fcd2ec37eab",
+    "url": "/static/js/runtime~main.369d624f.js"
+  },
+  {
+    "revision": "be36462b27618d0b33459165cbe50911",
+    "url": "/static/media/hugo-location-access.be36462b.png"
+  },
+  {
+    "revision": "bf666145265b166eca68827cdee8f61b",
+    "url": "/static/media/hugo-navigation-support.bf666145.png"
+  },
+  {
+    "revision": "d8004ba7f1c715c6c4eed91682af2d01",
+    "url": "/static/media/img_mac.d8004ba7.png"
+  },
+  {
+    "revision": "ce56ce3e00467d940e9b0452dbf83565",
+    "url": "/static/media/kid.ce56ce3e.jpg"
+  },
+  {
+    "revision": "5394522a82098a6e6371bd5f408e079d",
+    "url": "/static/media/logo.5394522a.png"
+  },
+  {
+    "revision": "ade73e55cdec2800237d62fbda79202b",
+    "url": "/static/media/parallax.ade73e55.jpg"
+  },
+  {
+    "revision": "ce36dd8f06fb1aa6852056e5b7940120",
+    "url": "/static/media/signup_driver.ce36dd8f.png"
+  },
+  {
+    "revision": "9c5a2f244a1fefff76efa604d259da74",
+    "url": "/static/media/signup_passenger.9c5a2f24.png"
+  }
+]);
